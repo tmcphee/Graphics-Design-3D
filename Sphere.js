aa1b4319271ec4a2514dfd3ea0cb9ce2f31f851a
Troy Walther <tdgames29@gmail.com>
2020-03-24 15:23:12 -0400

    Added Generation and Drawing of sphere

--- Sphere.js
+++ Sphere.js
@@ -1,3 +1,5 @@
+var PI = 3.14;
+
 class Sphere{
     constructor(x, y, z, radius, shader, gl) {
         /*Initalise Variables such as X position Y position, GL context, radius, pointer 
@@ -12,25 +14,62 @@ class Sphere{
         this.vbo;
         this.petri = false;
         this.indexbuffer;
-        this.vertices = [x, y, 0];
-        this.indices = [0];
+        this.normals = []; //Definately not correct, fix later.
+        this.vertices = [];
+        this.indices = [];
         this.numIndices = 0;
-        this.colour = [x + 0.2, y + 0.2, 0.5, 1];
+        this.colour = [x, y, z];
         this.complete = false;
         this.poison = false;
         //Generate points of the sphere
         this.generate();
 
         //Generate vertex buffer and index buffer
-        //this.genBuffers(gl);
+        this.genBuffers(gl);
     }
     
-    genereate(){
-        for(var i = 0; i < 360; i++){
-            for(var j = 0; j < 360; j++){
-                
-            }
+    generate() {
+    // Initialization
+        var vertices, indices;
+        var SPHERE_DIV = 12;
+        var i, ai, si, ci;
+        var j, aj, sj, cj;
+        var p1, p2;
+  
+        // Vertices
+        var vertices = [], indices = [];
+        for (j = 0; j <= SPHERE_DIV; j++) {
+          aj = j * Math.PI / SPHERE_DIV;
+          sj = Math.sin(aj);
+          cj = Math.cos(aj);
+          for (i = 0; i <= SPHERE_DIV; i++) {
+            ai = i * 2 * Math.PI / SPHERE_DIV;
+            si = Math.sin(ai);
+            ci = Math.cos(ai);
+  
+            vertices.push(si * sj);  // X
+            vertices.push(cj);       // Y
+            vertices.push(ci * sj);  // Z
+          }
         }
+        this.vertices = new Float32Array(vertices);
+  
+        // Indices
+        for (j = 0; j < SPHERE_DIV; j++) {
+          for (i = 0; i < SPHERE_DIV; i++) {
+            p1 = j * (SPHERE_DIV+1) + i;
+            p2 = p1 + (SPHERE_DIV+1);
+  
+            indices.push(p1);
+            indices.push(p2);
+            indices.push(p1 + 1);
+  
+            indices.push(p1 + 1);
+            indices.push(p2);
+            indices.push(p2 + 1);
+          }
+        }
+        this.indices = new Uint16Array(indices);
     }
 
     setx(x) {
@@ -73,6 +112,43 @@ class Sphere{
     }
     getVertices() {
         return this.vertices;
+    }
+
+    genBuffers(gl) {
+        //Generate a vertex buffer and give it our vertices
+        this.vbo = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.vbo);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.vertices), gl.STATIC_DRAW);
+        gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+        //Generate a new index buffer and give it our indices
+        this.indexbuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexbuffer);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(this.indices), gl.STATIC_DRAW);
+    }
+
+    draw(canvas) {
+        
+        //define local "gl" variable so it's easier to call, same for shader
+        var gl = this.gl;
+        var shaderProgram = this.shaderProgram;
+        //Bind programs
+        gl.useProgram(shaderProgram);
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.vbo);
+
+        //Set position
+        var vpos = gl.getAttribLocation(shaderProgram, "vposition");
+        gl.vertexAttribPointer(vpos, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(vpos);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexbuffer);
+        var fColorLocation = gl.getUniformLocation(shaderProgram, "fColor");
+        gl.uniform4f(fColorLocation, this.colour[0], this.colour[1], this.colour[2], this.colour[3]);
+
+        // Set the view port
+        gl.viewport(0, 0, canvas.width, canvas.height);
+
+        //Draw the triangle
+        gl.drawElements(gl.TRIANGLES, this.indices.length, gl.UNSIGNED_SHORT, 0);
     }
 
 }
---
+++ index.js
@@ -0,0 +1,58 @@
+function main() {
+    console.log("program start")
+    const canvas = document.querySelector("#glCanvas");
+    const scoreCanvas = document.getElementById("score");
+    const goCanvas = document.getElementById("gameOver");
+    // Initialize the GL context and text contexts
+    const gl = canvas.getContext("webgl");
+    const ctx = scoreCanvas.getContext("2d");
+    const goctx = goCanvas.getContext("2d");
+    goCanvas.style.display = "none";
+    //Initialise vertex shader
+    var vs =
+      'attribute vec3 vposition;'
+      + 'void main(){'
+      + '  gl_Position = vec4(vposition, 1.0);'
+      + '}';
+    //initialise fragment shader
+    var fs =
+      'precision mediump float;' +
+      'uniform vec4 fColor;' +
+      ' void main(){' +
+      '' +
+      ' gl_FragColor = fColor;'
+      + '}'
+    //Compile and attach shader to GL context
+    var vshader = createShader(gl, vs, gl.VERTEX_SHADER);
+    var fshader = createShader(gl, fs, gl.FRAGMENT_SHADER);
+    var program = gl.createProgram();
+    gl.attachShader(program, vshader);
+    gl.attachShader(program, fshader);
+    gl.linkProgram(program);
+    gl.clearColor(0.0, 0.5, 0.0, 1.0);
+    
+    //Function to create a shader from input text
+    function createShader(gl, sourceCode, type) {
+      // Compiles either a shader of type gl.VERTEX_SHADER or gl.FRAGMENT_SHADER
+      var shader = gl.createShader(type);
+      gl.shaderSource(shader, sourceCode);
+      gl.compileShader(shader);
+  
+      if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        var info = gl.getShaderInfoLog(shader);
+        throw 'Could not compile WebGL program. shader \n\n' + info;
+      }
+      console.log("shader compiled succesfully!");
+      return shader;
+    }
+  
+    //Create Circles
+    var spheres = [new Sphere(0, 0, 0,  0.6, program, gl)];
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    spheres[0].draw(canvas);
+  }
+  
+  window.onload = main;
+  
+  
