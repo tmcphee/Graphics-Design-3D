f7c9fcd2dea2b11dcf7d4a1f09cdf521b3a1eda9
connoralittle <57184344+connoralittle@users.noreply.github.com>
2020-03-26 13:59:34 -0400

    rotation working

--- Camera.js
+++
@@ -1,4 +0,0 @@
-class Camera {
-    matrix = mat4.create()
-
-}
--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+var near = -1;
+var far = 1;
+var radius = 1;
+var theta  = 0.0;
+var phi    = 0.0;
+
+var left = -1.0;
+var right = 1.0;
+var ytop = 1.0;
+var bottom = -1.0;
+
+var modelViewMatrix, projectionMatrix;
+var modelViewMatrixLoc, projectionMatrixLoc;
+var eye;
+const at = vec3(0.0, 0.0, 0.0);
+const up = vec3(0.0, 1.0, 0.0);
+
 function main() {
     console.log("program start")
     const canvas = document.querySelector("#glCanvas");
@@ -8,12 +25,15 @@ function main() {
     const ctx = scoreCanvas.getContext("2d");
     const goctx = goCanvas.getContext("2d");
     goCanvas.style.display = "none";
+
     //Initialise vertex shader
     var vs =
       'attribute vec3 vposition;'
+      +'uniform mat4 projectionMatrix;'
+      +'uniform mat4 modelViewMatrix;'
       +'varying lowp vec4 vColor;'
       + 'void main(){'
-      + '  gl_Position = vec4(vposition, 1.0);'
+      + '  gl_Position = modelViewMatrix * vec4(vposition, 1.0);'
       +'vColor = vec4(vposition, 1.0);'
       + '}';
     //initialise fragment shader
@@ -26,10 +46,16 @@ function main() {
     //Compile and attach shader to GL context
     var vshader = createShader(gl, vs, gl.VERTEX_SHADER);
     var fshader = createShader(gl, fs, gl.FRAGMENT_SHADER);
-    var program = gl.createProgram();
+    var program = gl.createProgram(); 
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    
+    gl.enable(gl.DEPTH_TEST);
+
     gl.attachShader(program, vshader);
     gl.attachShader(program, fshader);
     gl.linkProgram(program);
+    gl.useProgram(program)
     gl.clearColor(0.0, 0.5, 0.0, 1.0);
     
     //Function to create a shader from input text
@@ -51,12 +77,42 @@ function main() {
     var spheres = [new Sphere(0, 0, 0, 0.6, program, gl)];
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    spheres[0].draw(canvas);
+    console.log()
 
-    const matrix = mat4.create()
-    console.log(matrix)
+    modelViewMatrixLoc = gl.getUniformLocation( program, "modelViewMatrix" );
+    projectionMatrixLoc = gl.getUniformLocation( program, "projectionMatrix" );
+
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    eye = vec3(radius*Math.sin(phi), radius*Math.sin(theta), 
+             radius*Math.cos(phi));
+             
+    modelViewMatrix = lookAt(eye, at , up);
+    projectionMatrix = ortho(left, right, bottom, ytop, near, far);
+    
+    gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelViewMatrix) );
+    gl.uniformMatrix4fv( projectionMatrixLoc, false, flatten(projectionMatrix) );
+
+    console.log(modelViewMatrix)
+    spheres[0].draw(canvas)
+
+    window.requestAnimationFrame(animate);
+
+    function animate(time) {
+      gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+      eye = vec3(radius*Math.sin(theta)*Math.cos(phi), 
+        radius*Math.sin(theta)*Math.sin(phi), radius*Math.cos(theta));
+
+        modelViewMatrix = mult(modelViewMatrix, rotate(1, [1,1,0]))
+      projectionMatrix = ortho(left, right, bottom, ytop, near, far);
+
+      gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelViewMatrix) );
+      gl.uniformMatrix4fv( projectionMatrixLoc, false, flatten(projectionMatrix) );
+
+      spheres[0].draw(canvas);
+      window.requestAnimationFrame(animate);
+    }
   }
   
   window.onload = main;
-  
   
